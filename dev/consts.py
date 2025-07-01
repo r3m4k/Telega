ccbfa44c9c89f4a2aa38fd518043a07f5471d72d
r3m4k <romanovskiy_roma_2004@mail.ru>
2025-07-01 17:41:53 +0300

    Создал класс для гибкого создания графиков и работы с ними

--- dev/consts.py
+++ dev/consts.py
@@ -10,3 +10,13 @@ else:
     raise RuntimeError('Unsupported OS')
 
 JSON_FILE = f'{CWD}/dev/settings.json'
+
+color_scheme = {
+            'RGB_classic': {'x': 'tab:blue', 'y': 'tab:red', 'z': 'tab:green'},
+            'RGB_dark': {'x': 'navy', 'y': 'maroon', 'z': 'darkgreen'},
+            'RGB_light': {'x': 'skyblue', 'y': 'coral', 'z': 'yellowgreen'},
+
+            'COP_classic': {'x': 'tab:cyan', 'y': 'tab:orange', 'z': 'tab:purple'},
+            'COP_dark': {'x': 'steelblue', 'y': 'goldenrod', 'z': 'purple'},
+            'COP_light': {'x': 'lightblue', 'y': 'orange', 'z': 'hotpink'},
+        }
