24c51848c04fa495011f83b59f09f4107f2f68ed
r3m4k <romanovskiy_roma_2004@mail.ru>
2025-08-13 17:31:09 +0300

    Реализовал алгоритм учёта вращения платы
    Нужно сделать вычисление скорости и понять адекватность полученных результатов

--- dev/rotation.py
+++ dev/rotation.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+# User imports
+from consts import Moscow_coordinates
+from data_analys import trapezoid_integration
+
 ##########################################################
 
 class Matrix:
@@ -27,6 +31,10 @@ class Matrix:
     def __imul__(self, matrix):
         if isinstance(matrix, Matrix):
             self._matrix = np.dot(self._matrix, matrix._matrix)
+            return self
+        elif isinstance(matrix, np.ndarray):
+            self._matrix = np.dot(self._matrix, matrix)
+            return self
         else:
             raise ValueError('In-place multiplication for Matrix is only supported with Matrix')
 
@@ -43,6 +51,9 @@ class Matrix:
     # -------------------------------
     # Дополнительный функционал
     # -------------------------------
+    def inv(self):
+        return np.linalg.inv(self._matrix)
+
     def set_rotation_to_angles(self, X_angle: float, Y_angle: float, Z_angle: float):
         """
         Заполняет матрицу поворота 3x3 для углов Эйлера в порядке X → Y → Z.
@@ -126,7 +137,7 @@ class Vector:
     """
     Класс, предназначенный для работы с вектором, имеющий три компоненты
     """
-    def __init__(self, coords: list[float | np.float64]):
+    def __init__(self, coords: list[np.floating | float]):
         if len(coords) != 3:
             raise RuntimeError('Incorrect dimension of the vector.'
                                f'The Vector type have 3 dimensions, but given {len(coords)}')
@@ -166,15 +177,18 @@ class Vector:
             self._X_coord += other._X_coord
             self._Y_coord += other._Y_coord
             self._Z_coord += other._Z_coord
+
+            return self
+
         else:
             raise ValueError('Term must be Vector')
 
     def __sub__(self, other):
         if isinstance(other, Vector):
             return Vector([
-                self._X_coord + other._X_coord,
-                self._Y_coord + other._Y_coord,
-                self._Z_coord + other._Z_coord
+                self._X_coord - other._X_coord,
+                self._Y_coord - other._Y_coord,
+                self._Z_coord - other._Z_coord
             ])
         else:
             raise ValueError('Reduced and subtracted must be Vector')
@@ -184,6 +198,8 @@ class Vector:
             self._X_coord -= other._X_coord
             self._Y_coord -= other._Y_coord
             self._Z_coord -= other._Z_coord
+
+            return self
         else:
             raise ValueError('Term must be Vector')
 
@@ -209,13 +225,15 @@ class Vector:
         self._Y_coord *= _multiplier
         self._Z_coord *= _multiplier
 
+        return self
+
     # -------------------------------
     # Работа с коллекциями
     # -------------------------------
     def __len__(self):
         return 3
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: int | str):
         if isinstance(item, int):
             return self.to_list()[item]
 
@@ -272,37 +290,117 @@ class Vector:
 
 ##########################################################
 
-class RotatedValues:
+class RotatingFrameAnalyzer:
     """
-    Класс для перевода данных акселерометра из СК датчика в СК Земли.
+    Класс для учёта нулевых значений полученных величин и вращения платы с помощью построения матриц поворота.
+    Данные акселерометра переводятся в СК Земли, а из данных гироскопа вычитаются нулевые значения с учётом поворота
+    платы относительно начального положения.
+    Таким образом, получим абсолютные значения угловых скоростей на протяжении всего проезда.
+
     Параметры СК Земли: OX - на восток, OY - на север, OZ - вертикально вверх.
     """
     def __init__(self,
-                 acc_dict: dict[str, np.typing.NDArray], acc_dict_buffer: dict[str, np.typing.NDArray],
-                 gyro_dict: dict[str, np.typing.NDArray], gyro_dict_buffer: dict[str, np.typing.NDArray],
+                 time: np.typing.NDArray,
+                 acc_dict: dict[str, np.typing.NDArray[np.floating]],
+                 acc_dict_buffer: dict[str, np.typing.NDArray[np.floating]],
+                 gyro_dict: dict[str, np.typing.NDArray[np.floating]],
+                 gyro_dict_buffer: dict[str, np.typing.NDArray[np.floating]],
                  latitude: float):
 
         self._coords = ['X', 'Y', 'Z']
+        self._data_size = len(acc_dict['X'])
+
         dicts = [acc_dict, acc_dict_buffer, gyro_dict, gyro_dict_buffer]
         if not all(list(d.keys()) == self._coords for d in dicts):
             raise KeyError('Ключи в переданных словарях не совпадают с ["X", "Y", "Z"]')
 
-        self._acc: dict[str, np.typing.NDArray] = acc_dict
-        self._acc_buffer: dict[str, np.typing.NDArray] = acc_dict_buffer
+        self._analyzed_values = {
+            'Acc_X': list(), 'Acc_Y': list(), 'Acc_Z': list(),
+            'Gyro_X': list(), 'Gyro_Y': list(), 'Gyro_Z': list(),
+        }
 
-        self._gyro: dict[str, np.typing.NDArray] = gyro_dict
-        self._gyro_buffer: dict[str, np.typing.NDArray] = gyro_dict_buffer
+        self._time_array = time
+
+        self._acc_array: list[Vector] = [Vector([acc_dict[coord][i] for coord in self._coords]) for i in range(self._data_size)]
+        self._acc_zero = Vector([np.mean(acc_dict_buffer[coord]) for coord in self._coords])
+
+        self._gyro_array: list[Vector] = [Vector([gyro_dict[coord][i] for coord in self._coords]) for i in range(self._data_size)]
+        self._gyro_zero = Vector([np.mean(gyro_dict_buffer[coord]) for coord in self._coords])
 
         self._latitude: float = latitude        # Широта, на которой проводились измерения
 
-        self._R_to_prev = np.eye(3)             # Матрица перехода к предыдущему состоянию
-        self._R_to_Earth = np.eye(3)            # Матрица перехода из исходного состояния в СК Земли, в которой ось ОУ направлена на север
+        self._R_to_next = Matrix()             # Матрица перехода к следующему состоянию
+        self._R_to_begin = Matrix()            # Матрица перехода к исходному состоянию
+        self._R_to_Earth = Matrix()            # Матрица перехода из исходного состояния в СК Земли, в которой ось ОУ направлена на север
 
-        self.R_det = []         # Список значений определителей матрицы перехода, нужен для контроля корректности алгоритма
+    def get_analyzed_values(self):
+        return {key: np.asarray(self._analyzed_values[key]) for key in self._analyzed_values.keys()}
 
+    def start(self):
+        """
+        Принцип работы алгоритма:
 
-    def _start(self):
-        pass
+        1) Вычисляем реальные значения ускорения и угловой скорости на (i-1)-ом и i-ом шаге, путём вычитания нулевого значения на (i-1)-ом шаге.
+        2) По полученным значениям угловой скорости строим матрицу поворота от (i-1)-го состояния к i-му состоянию.
+        3) Умножаем нулевые вектора величин на полученную матрицу перехода. Тем самым, мы получаем нулевые значения в новом положении платы.
+        4) Данные угловой скорости оставляем в таком виде, а данные ускорения последовательно умножаем на матрицы R_to_begin и R_to_Earth.
+            Таким образом мы получаем данные угловой скорости вдоль направления движения, а данные ускорений в СК Земли.
+        """
+        self._R_to_Earth.set_rotation_to_Earth(acc=self._acc_zero, gyro=self._gyro_zero, latitude_degree=self._latitude)
 
+        # Первый кадр измерений сохраним с вычитанием нулевых значений, ускорения переведём в СК Земли
+        acc = self._acc_array[0] - self._acc_zero
+        acc = self._R_to_begin * acc
+        acc = self._R_to_Earth * acc
+
+        gyro = self._gyro_array[0] - self._gyro_zero
+
+        for coord in self._coords:
+            self._analyzed_values[f'Acc_{coord}'].append(acc[coord])
+            self._analyzed_values[f'Gyro_{coord}'].append(gyro[coord])
+
+        for i in range(1, self._data_size):
+            # Вектора ускорения и угловой скорости на данной и предыдущей итерации
+            acc = self._acc_array[i]
+
+            gyro = self._gyro_array[i]
+            gyro_prev = self._gyro_array[i-1]
+
+            # Считаем, что между текущим и предыдущем измерениями плата повернулась на малый угол,
+            # поэтому нулевые значения векторов не изменятся на одном шаге
+            acc -= self._acc_zero
+
+            gyro -= self._gyro_zero
+            gyro_prev -= self._gyro_zero
+
+            # Вычислим матрицу поворота от состояния i-1 к состоянию i
+            # Углы поворота
+            angles = {key: trapezoid_integration(np.array([gyro_prev[key] / 1000, gyro[key] / 1000]), self._time_array[i] - self._time_array[i-1])
+                      for key in self._coords}
+
+            # Вычислим матрицу поворота к следующему состоянию
+            self._R_to_next.set_rotation_to_angles(X_angle=angles['X'], Y_angle=angles['Y'], Z_angle=angles['Z'])
+
+            # Вычислим матрицу поворота к первоначальному состоянию.
+            # Умножаем на обратную матрицу тк это матрица циклического поворота из i-го состояния в состояние i-1, и так далее до нулевого состояния
+            self._R_to_begin *= self._R_to_next.inv()
+
+            # Переведём нулевые величины в новое состояние
+            self._acc_zero = self._R_to_next * self._acc_zero
+            self._gyro_zero = self._R_to_next * self._gyro_zero
+
+            # Переведём значение ускорения в СК Земли
+            acc = self._R_to_begin * acc
+            acc = self._R_to_Earth * acc
+
+            # Сохраним полученные значения
+            for coord in self._coords:
+                self._analyzed_values[f'Acc_{coord}'].append(acc[coord])
+                self._analyzed_values[f'Gyro_{coord}'].append(gyro[coord])
 
 ##########################################################
+
+if __name__ == "__main__":
+    m = Matrix()
+    m.set_rotation_to_angles(0, 90, 90)
+    print(type(m.inv()))
